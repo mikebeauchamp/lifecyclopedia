b7833809beeb1579f5a864c9463da9eca7652ff2
Mike Beauchamp <mbeauchamp@minitab.com>
2024-07-03 15:36:53 -0400

    2024.07.03: intro to updating state

---
+++ src/CyclOPediaClassPage.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { getRandomUser } from "./Utility/api";
+
+export default class CyclOPediaClassPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  }
+
+  componentDidMount = async () => {
+    console.log("Component Did Mount");
+    // const response = await getRandomUser();
+    // console.log(response);
+    // this.setState((prevState) => {
+    //   return {
+    //     instructor: {
+    //       name: response.data.first_name + " " + response.data.last_name,
+    //       email: response.data.email,
+    //       phone: response.data.phone_number,
+    //     },
+    //   };
+    // });
+  };
+
+  componentDidUpdate() {
+    console.log("Component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount");
+  }
+
+  handleAddStudent = () => {
+    this.setState((prevState) => {
+      return {
+        studentCount: prevState.studentCount + 1,
+      };
+    });
+  };
+
+  handleRemoveAllStudents = () => {
+    this.setState((prevState) => {
+      return {
+        studentCount: 0,
+      };
+    });
+  };
+
+  render() {
+    console.log("Render Component");
+    return (
+      <div>
+        {this.state.instructor && (
+          <div className="p-3">
+            <span className="h4 text-success">Instructor</span>
+            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+            <br />
+            Name: {this.state.instructor.name}
+            <br />
+            Email: {this.state.instructor.email}
+            <br />
+            Phone: {this.state.instructor.phone}
+            <br />
+          </div>
+        )}
+        <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input type="text" placeholder="Name..."></input>
+          <br />
+          <textarea placeholder="Feedback..."></textarea>
+        </div>
+        <div className="p-3">
+          <span className="h4 text-success">Students</span>
+          <div>Student Count: {this.state.studentCount}</div>
+          <button
+            className="btn btn-success btn-sm"
+            onClick={() => this.handleAddStudent()}
+          >
+            Add Student
+          </button>
+          &nbsp;
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => this.handleRemoveAllStudents()}
+          >
+            Remove Student
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,20 @@
+import logo from "../images/react.png";
+
+function MainHeader() {
+  return (
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
+      <img src={logo} style={{ height: "35px", verticalAlign: "top" }}></img>
+      <span className="h2 pt-4 m-2 text-white-50">LifeCyclOPedia</span>
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div>
+      <MainHeader />
+    </div>
+  );
+};
+
+export default Header;
